b06e19e07582801a0562d63cdea95fe11dfe9e7c
gabriel.land <gabriel.land@qi.edu.br>
2022-11-26 21:44:48 -0300

    ajustes

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
         <vs-input
           v-model="cep"
           placeholder="digite o CEP aqui"
+          v-mask="'#####-###'"
         />
       </vs-col>
 
@@ -16,7 +17,32 @@
       </vs-col>
     </vs-row>
 
-    <p>{{address}}</p>
+    <vs-row v-show="address">
+      <vs-col>        
+        <fieldset>
+          <legend>Dados</legend>
+          <vs-list>
+            <vs-list-item title="CEP" :subtitle="zipCode"></vs-list-item>
+            <vs-list-item title="LOGRADOURO" :subtitle="publicPlace"></vs-list-item>
+            <vs-list-item title="COMPLEMENTO" :subtitle="complement"></vs-list-item>
+            <vs-list-item title="BAIRRO" :subtitle="district"></vs-list-item>
+            <vs-list-item title="LOCALIDADE" :subtitle="location"></vs-list-item>
+            <vs-list-item title="UF" :subtitle="state"></vs-list-item>
+            <vs-list-item title="IBGE" :subtitle="ibge"></vs-list-item>
+            <vs-list-item title="GIA" :subtitle="gia"></vs-list-item>
+            <vs-list-item title="DDD" :subtitle="ddd"></vs-list-item>
+            <vs-list-item title="SIAFI" :subtitle="siafi"></vs-list-item>
+          </vs-list>          
+        </fieldset>
+      </vs-col>
+    </vs-row>    
+
+    <vs-popup
+      title="CEP INVALIDO"
+      :active.sync="popupError"
+    >
+      <p>CEP informado não trouxe nenhum resultado, por favor verifique e tente novamente</p>
+    </vs-popup>
   </div>
 </template>
 
@@ -29,6 +55,17 @@ export default {
     cep: null,
     address : null,
     error: null,
+    zipCode: null,
+    publicPlace: null,
+    complement: null,
+    district: null,
+    location: null,
+    state: null,
+    ibge: null,
+    gia: null,
+    ddd: null,
+    siafi: null,
+    popupError: false,
   }),
   created () {
 		document.title = "Web Address";
@@ -41,11 +78,30 @@ export default {
       .getAddress(this.cep)
       .then(({ data }) => {
         this.address = Array.isArray(data) ? data : [data];
+        console.log(this.address)
+
+        if (this.address[0].erro == true) {
+          this.address = null;
+          this.popupError = true;  
+        } else{
+          this.zipCode = this.address[0].cep;
+          this.publicPlace = this.address[0].logradouro;
+          this.complement = this.address[0].complemento;
+          this.district = this.address[0].bairro;
+          this.location = this.address[0].localidade;
+          this.state = this.address[0].uf;
+          this.ibge = this.address[0].ibge;
+          this.gia = this.address[0].gia == "" ? "Não Disponivel" : this.address[0].gia;
+          this.ddd = this.address[0].ddd;
+          this.siafi = this.address[0].siafi;
+        }        
       })
       .catch(({ response }) => {
-        if (response && response.data) {
-            this.error = response.data;
-            console.log(response);
+        this.address = null;
+        this.popupError = true;
+        if (response && response.data) {          
+          this.error = response.data;
+          console.log(response);
         }
       })
       .finally(() => {
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import Vuesax from 'vuesax'
+import VueMask from 'v-mask'
 
 import 'vuesax/dist/vuesax.css'
+
+Vue.use(VueMask);
 Vue.use(Vuesax, {
 })
 
